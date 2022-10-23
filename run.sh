bb1a9011739989b36712e0a5865afc45d750dcda
foxxy <hoanganh6758@gmail.com>
2022-10-23 16:25:14 +0700

    modified run.sh

--- run.sh
+++ run.sh
@@ -9,12 +9,6 @@ pip3 install pygame
 # pygame.display.list_modes()
 # import os
 # os.environ["SDL_VIDEODRIVER"] = "dummy"
-sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
-sdl-config --cflags --libs
-python config.py
-python config.py
-sudo python setup.py install
-sudo python setup.py
 
 cd ./source && python3 main.py 
 exit;
