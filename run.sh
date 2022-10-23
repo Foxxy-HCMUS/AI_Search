f0a8d4452d9457b13773762f407bfd2b0dcae8e8
foxxy <hoanganh6758@gmail.com>
2022-10-23 16:24:10 +0700

    create bash file

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
-#!/user/bin/ python
-cd ./source
-sudo apt install python3-pip
-#python3 $1 $2
-pip install numpy
-python3 main.py 
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get install python3 python3-pip idle3 git 
+pip3 install numpy 
+pip3 install pygame 
+
+#import pygame
+# pygame.init()
+# pygame.display.list_modes()
+# import os
+# os.environ["SDL_VIDEODRIVER"] = "dummy"
+sudo apt-get install libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev
+sdl-config --cflags --libs
+python config.py
+python config.py
+sudo python setup.py install
+sudo python setup.py
+
+cd ./source && python3 main.py 
 exit;
